78f5dcaf2c4dbc9c5c333d9216963d03e6557fd2
morfey1 <morfey__@abv.bg>
2016-04-07 16:01:34 +0300

    Added files via upload

---
+++ Print Sequence/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Print_Sequence
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 2; i < 1000; i++)
+            {
+                if (i% 2 !=0)
+                {
+                    Console.WriteLine(i*(-1));
+                }else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
